61d76c7a5eea28f333b20a2956ead1ee510c6798
K1 <K1@DESKTOP-VJ74ISC>
2023-05-06 13:27:39 +0430

    Specify a timeout to prevent deadlocks

---
+++ AvoidingDeadlocks/Program.cs
@@ -0,0 +1,63 @@
+﻿using static System.Console;
+using AvoidingDeadlocks;
+using System.Diagnostics;
+
+WriteLine("Please wait for the tasks to complete.");
+Stopwatch watch = Stopwatch.StartNew();
+
+Task a = Task.Factory.StartNew(MethodA);
+Task b = Task.Factory.StartNew(MethodB);
+
+Task.WaitAll(new Task[] { a, b });
+
+WriteLine();
+WriteLine($"Results: {SharedObjects.Message}.");
+WriteLine($"{watch.ElapsedMilliseconds:N0} elapsed milliseconds.");
+
+static void MethodA()
+{
+    try
+    {
+        if (Monitor.TryEnter(SharedObjects.Conch, TimeSpan.FromSeconds(15)))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(SharedObjects.Random.Next(2000));
+                SharedObjects.Message += "A";
+                Write(".");
+            }
+        }
+        else
+        {
+            WriteLine("Method A timed out when entering a monitor on conch.");
+        }
+    }
+    finally
+    {
+        Monitor.Exit(SharedObjects.Conch);
+    }
+}
+
+static void MethodB()
+{
+    try
+    {
+        if (Monitor.TryEnter(SharedObjects.Conch, TimeSpan.FromSeconds(15)))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(SharedObjects.Random.Next(2000));
+                SharedObjects.Message += "B";
+                Write(".");
+            }
+        }
+        else
+        {
+            WriteLine("Method B timed out when entering a monitor on conch.");
+        }
+    }
+    finally
+    {
+        Monitor.Exit(SharedObjects.Conch);
+    }
+}
---
+++ AvoidingDeadlocks/SharedObjects.cs
@@ -0,0 +1,8 @@
+﻿namespace AvoidingDeadlocks;
+
+public static class SharedObjects
+{
+    public static Random Random = new();
+    public static string? Message; // a shared resource
+    public static object Conch = new();
+}
