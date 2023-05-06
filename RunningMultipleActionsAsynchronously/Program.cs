66e350be87f91291564981e7591669203c7f1602
K1 <K1@DESKTOP-VJ74ISC>
2023-05-06 12:55:28 +0430

    asynchronously using tasks

--- RunningMultipleActionsAsynchronously/Program.cs
+++ RunningMultipleActionsAsynchronously/Program.cs
@@ -19,6 +19,7 @@ WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
 static void OutputThreadInfo()
 {
     Thread t = Thread.CurrentThread;
+
     WriteLine(
         "Thread Id: {0}, Priority: {1}, Background: {2}, Name: {3}",
         t.ManagedThreadId, t.Priority,
